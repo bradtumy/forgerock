f35b7de4d4db99014ceb5c02cd8d7d22e346de62
Brad Tumy <brad.tumy@gmail.com>
2013-10-25 17:10:28 -0400

    cleaned up for better portability

--- OpenAM/checkEntitlements.sh
+++ OpenAM/checkEntitlements.sh
@@ -2,34 +2,36 @@
 # This script will authenticate a user and then check their
 # entitlements on a resource url that is provided to it.
 
+#import settings
+source settings
+
 echo -e "Enter the UID of the subject to authenticate:"
 read uid
 echo -e "Enter the subject's password:"
 read -s pass
-echo -e "Enter the OpenAM's Server FQDN incl the OpenAM Context:"
-read server
-echo -e "Enter the protected resource URL:"
-read resource
 echo -e "How many times should I check?"
 read count
 
 #Authenticate
-url1="$server/identity/authenticate"
-content="$(curl -s --request POST --data "username=$uid&password=$pass" $url1)"
-#echo $content
+URL="$PROTOCOL://$OPENAM_SERVER:$OPENAM_SERVER_PORT/openam/identity/authenticate"
+DATA="username=$uid&password=$pass"
+RESOURCE=$PROTECTED_RESOURCE
+
+TOKEN="$(curl -s --request POST --data $DATA $URL)"
+#echo $TOKEN
 
 # Check to see if authn was successful, stop script if not
-if [[ $content == t* ]];
+if [[ $TOKEN == t* ]];
 then 
   echo -e "Authentication Successful"'\n'
-  token=${content:9} # Capture the substr after "tokenid="
+  TOKEN1=${TOKEN:9} # Capture the substr after "tokenid="
   #echo -e "Token for authenticated user:"
   #echo -e $token'n' # for debugging, display the tokenid
   sleep 2
   echo -e "Checking Entitlements: "'\n'
   sleep 1
-  url="$server/identity/authorize?uri=$resource&action=GET&subjectid="
-  myUrl=$url$token
+  url="$PROTOCOL://$OPENAM_SERVER:$OPENAM_SERVER_PORT/openam/identity/authorize?uri=$RESOURCE&action=GET&subjectid="
+  myUrl=$url$TOKEN1
   echo -e "Here is the command I am going to run:\n"
   echo -e $myUrl'\n' # for debugging, show myUrl
   sleep 3
