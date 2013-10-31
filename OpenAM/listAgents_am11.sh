1b260d6b0d9d3f4030bb136158dc825d34ec1bc4
bradtumy <brad.tumy@gmail.com>
2013-10-31 09:38:40 -0400

    Update listAgents_am11.sh

--- OpenAM/listAgents_am11.sh
+++ OpenAM/listAgents_am11.sh
@@ -17,12 +17,12 @@ AUTH_URL="$BASE_URL/identity/authenticate"
 AUTH_DATA="username=amadmin&password=$pass"
 AUTH_TOKEN="$(curl -s --data $AUTH_DATA $AUTH_URL)"
 AUTH_TOKEN2=${AUTH_TOKEN:9}
-echo "Auth Token: " $AUTH_TOKEN2
+#echo "Auth Token: " $AUTH_TOKEN2
 
 # SEARCH for the names of all agents, matching the filter
 
 URL="$BASE_URL/json/agents?_queryID=$FILTER"
 #URL="$BASE_URL/identity/search" ##legacy API
-echo "URL: " $URL
+#echo "URL: " $URL
 TOKEN="$(curl -s --header "iPlanetDirectoryPro: $AUTH_TOKEN2" --header "Content-Type: application/json" $URL)"
 echo $TOKEN
